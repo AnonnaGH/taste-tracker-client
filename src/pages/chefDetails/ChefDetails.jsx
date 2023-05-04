378ac18961e5ed58e532956df17d3e860b8a4792
“AnonnaGH” <anonnaferdosh193@gmail.com>
2023-05-04 16:27:35 +0600

    chef banner display

--- src/pages/chefDetails/ChefDetails.jsx
+++ src/pages/chefDetails/ChefDetails.jsx
@@ -11,12 +11,27 @@ const ChefDetails = () => {
     return (
         <div>
             <div>
-                <h1>chaf detsils</h1>
-                <img
-                    className="img-fluid rounded-start"
-                    src={chefRecipe?.selectedChef?.picture}
-                    alt=""
-                />
+                <h1 className='text-center text-success alert alert-success mt-3'>Chef Details</h1>
+                <div className='row my-4'>
+                    <div className='col-md-4 text-center'>
+                        <img
+                            className="img-fluid rounded-start"
+                            src={chefRecipe?.selectedChef?.picture}
+                            alt=""
+                        />
+
+                    </div>
+                    <div className="col-md-8 pe-5">
+                        <div className="card-body">
+                            <h3 className="card-title mb-3 text-success alert alert-success">{chefRecipe?.selectedChef?.name}</h3>
+                            <p>{chefRecipe?.selectedChef?.bio}</p>
+                            <hr />
+                            <p className='fw-bold'> Recipes: {chefRecipe?.selectedChef?.recipes} recipes</p>
+                            <p className='fw-bold'>Experience: {chefRecipe?.selectedChef?.experience} Years</p>
+                            <p className='fw-bold'> Likes: {chefRecipe?.selectedChef?.likes}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 {chefRecipe?.selectedRecipe?.map((recipe) => (
